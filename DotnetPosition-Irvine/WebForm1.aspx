cc5b0dfeded2f14818d2ba4dc1a32e306da06cbb
shubham <shubham@SHUBHAM>
2018-04-04 20:32:50 +0530

    Add project files.

---
+++ DotnetPosition-Irvine/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DotnetPosition_Irvine.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table style="width: 40%;">
+            <tr>
+                <td>
+                    <b>Second Longest Word : </b>
+                </td>
+                <td>
+                    <asp:Label ID="lblWord" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <b>Total Count :</b>
+                </td>
+                <td>  <asp:Label ID="lblCount" runat="server"></asp:Label></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DotnetPosition-Irvine/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DotnetPosition_Irvine
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // TextReader tr = new StreamReader("wordlist.txt");
+            var path = Server.MapPath(@"~/wordlist.txt");
+            TextReader sr = new StreamReader(path);
+            String line = sr.ReadToEnd();
+            string[] lstArray = line.Replace(System.Environment.NewLine, " ").Split(' ');
+
+            List<string> Defaultlist = new List<string>(lstArray);
+            List<string> Mainlist = new List<string>();
+            char[] smallstrchar = String.Join("", Defaultlist.Where(x => x.Length <= 4)).ToCharArray();
+            char[] bigstrchar = String.Join("", Defaultlist.Where(x => x.Length > 4)).ToCharArray();
+            char[] modchar = bigstrchar.Except(smallstrchar).ToArray();
+
+            foreach (string bigstr in Defaultlist)
+            {
+                if (!(bigstr.IndexOfAny(modchar) != -1))
+                {
+                    Mainlist.Add(bigstr);
+                }
+            }
+            Mainlist = Mainlist.OrderByDescending(x => x.Length).Take(2).ToList();
+           lblWord.Text = Mainlist[1];
+            lblCount.Text = Mainlist[1].Length.ToString();
+            //foreach (string finalstr in Finallist)
+            //{
+             
+            //    Console.WriteLine(finalstr);
+            //}
+        }
+
+    }
+
+  
+}
